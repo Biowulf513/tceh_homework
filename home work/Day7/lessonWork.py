b3ea8900f419bc74d8c14d8e0442220c9ca6cbfe
Biowulf <cherepanov92@gmail.com>
2018-02-22 16:44:38 +0500

    день7 тема декоратор

--- home work/Day7/lessonWork.py
+++ home work/Day7/lessonWork.py
@@ -4,41 +4,41 @@ def action_decorator(func):
         func(text)
 
     return inner
-#
-#
-# @action_decorator  # try to uncomment me
-# def shout(text):
-#     print(text.upper(), '!!!!')
-#
-#
-# @action_decorator
-# def whisper(text):
-#     print(text.lower(), '...')
-#
-#
+
+
+@action_decorator  # try to uncomment me
+def shout(text):
+    print(text.upper(), '!!!!')
+
+
+@action_decorator
+def whisper(text):
+    print(text.lower(), '...')
+
+
 @action_decorator
 def say(something):
     something += '; was said.'
     print(something)
+
+
+if __name__ == '__main__':
+    # Basic functions:
+    say('hi')
+    whisper('Hello')
+    say('i am here')
+
+
+# def dec_action(func):
+#     def modify(text):
+#         print('good')
+#         func(text)
 #
+#     return modify
 #
-# if __name__ == '__main__':
-#     # Basic functions:
-#     say('hi')
-#     whisper('Hello')
-say('i am here')
-
-
-def dec_action(func):
-    def modify(text):
-        print('good')
-        func(text)
-
-    return modify
-
-@dec_action
-def ok(message):
-    message += ' !!!'
-    print(message)
-
-ok('ok')
+# @dec_action
+# def ok(message):
+#     message += ' !!!'
+#     print(message)
+#
+# ok('ok')
