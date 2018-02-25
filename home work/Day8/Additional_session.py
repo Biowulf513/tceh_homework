33b1b715413069642c1b511b31ae01f6e360d3ea
Biowulf513 <cherepanov92@gmail.com>
2018-02-25 12:05:25 +0500

    day8 факультатив повтроили декораторы

--- home work/Day8/Additional_session.py
+++ home work/Day8/Additional_session.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
+# Декораторы
 def decorator(func):
     func.counter = 1
 
     def fake(value):
-        print('Runs:', func.counter)
+        print(func.__name__, 'Runs:', func.counter)
         func.counter += 1
         return 'fake:' + func(value)
 
@@ -17,5 +18,30 @@ def my_str(value):
 def i_str(value):
     return str(value)
 
-print(my_str(123))
-print(i_str([]))
+# print(my_str(123))
+# print(my_str(2335))
+# print(i_str([]))
+
+# пример 2
+
+import time
+
+def timer(func):
+
+    def sec(col):
+        start = time.clock()
+        result = func(col)
+        print('time:{}'.format(time.clock() - start))
+        return result
+
+    return sec
+
+
+@timer
+def my_range(col):
+    # time.sleep(5)
+    return list(x for x in range(col))
+
+my_range(1)
+
+# пример 2
