60d71716557743138aa74215c476e89bf969bd17
Biowulf <cherepanov92@gmail.com>
2018-02-26 10:28:23 +0500

    day8 Домашнее задание (Морской бой) работа с картой отрисовка карты

--- home work/Day8/sea_battle/map.py
+++ home work/Day8/sea_battle/map.py
@@ -2,7 +2,10 @@
 # Поле, свойства: размер, количество кораблей
 
 class map:
+
+
     def __init__(self):
+        self.graphic_array = {'none':'_', 'hit':'▪', 'dead':'⨯', 'ship':'▩', 'shadow':'⬫' }
         self.long_x = 10
         self.long_y = 10
     # Создание списка с поляим карты
@@ -12,14 +15,25 @@ class map:
         def map_generator():
             # перечень указателей столбцов
             letters = 'abcdefghij'
-            yield list('\\' + letters)
+            yield list('  ' + letters)
             for i in range(self.long_y):
-                array = list('_' * self.long_y)
-                array.insert(0, i + 1)# Пофиксить + 1
+                array = list(self.graphic_array['none'] * self.long_y)
+                if i < 9:
+                    array.insert(0, ' ' + str(i + 1) + ' ')# Пофиксить + 1
+                else:
+                    array.insert(0, str(i + 1) + ' ')  # Пофиксить + 1
                 yield array
 
         self.map_array = list(map_generator())
 
+    def show(self):
+        for line in self.map_array:
+            print(' '.join(line))
+
+    # def
+
+
+
 mappi = map()
 mappi.create()
-print(mappi.map_array)
+mappi.show()
