ef044dd2dcbe064630b83b0d33c7ae0875b6f3bb
Biowulf <Killimo513>
2018-02-27 22:01:39 +0500

    day8 + создаём поле + создаём игроков     + создание объекта игрок1     + создание объекта игрок2 + отображение поля +ход игроков     +ввод позиции         +провека вводимого значения         +преобразование буквенных значений в числовые         +проверка не занята ли позиция
        +установка знака на позицию
        +добавление символа в список победных комбинаций
        +проверка всех комбинаций с данной позицией
        +если комбинация чиста или в ней уже есть метка игрока: записать на позицию в комбинации символ игрока
        +если в комбинации есть метка другого игрока удалить комбинацию из списка
    
        +ход другого игрока
    +сообщение о победе
     обработка исключений

--- home work/Day8/XO2/main.py
+++ home work/Day8/XO2/main.py
@@ -18,20 +18,21 @@
     +если в комбинации есть метка другого игрока удалить комбинацию из списка
     
     +ход другого игрока
-сообщение о победе
++сообщение о победе
+ обработка исключений
 '''
 
 class XO:
     move_counter = 0
-    finish = False
-    win_result_list = [[[1, 1], [1, 2], [1, 3]],
-                       [[2, 1], [2, 2], [2, 3]],
-                       [[3, 1], [3, 2], [3, 3]],
-                       [[1, 1], [2, 1], [3, 1]],
-                       [[1, 2], [2, 2], [3, 2]],
-                       [[1, 3], [2, 3], [3, 3]],
-                       [[1, 1], [2, 2], [3, 3]],
-                       [[1, 3], [2, 2], [3, 1]],
+    game_play = True
+    win_result_list = [[[1,1],[1,2],[1,3],[]],
+                       [[2,1],[2,2],[2,3],[]],
+                       [[3,1],[3,2],[3,3],[]],
+                       [[1,1],[2,1],[3,1],[]],
+                       [[1,2],[2,2],[3,2],[]],
+                       [[1,3],[2,3],[3,3],[]],
+                       [[1,3],[2,2],[3,1],[]],
+                       [[1,1],[2,2],[3,3],[]],
                        ]
 
     def __init__(self):
@@ -42,24 +43,20 @@ class XO:
 
     # ход игрока
     def player_turn(self):
-        # while True:
         self.board.show_board()
         self.active_player = self.players_list[XO.move_counter % 2]
-        print('Ходит {}'.format(self.active_player.name))
+        print('Ходит {player} ({sign})'
+              .format(player = self.active_player.name, sign = self.active_player.sign ))
 
         position = self.check_turn(input('Введите позицию хода: '))
         if self.board.check_position(position):  # Проверка ячейки на занятость
             self.add_user_position(position)
             self.check_win_result(position)
             XO.move_counter += 1
-        #     break
-        # else:
-        #     continue
 
     # Добавление символа на игровую доску
     def add_user_position(self, position):
-        self.board.board_list[position[0]][position[1]] = self.active_player.sign
-        self.board.show_board()
+        self.board.board_list[position[1]][position[0]] = self.active_player.sign
 
     # провека вводимого значения
     # преобразование буквенных значений в числовые
@@ -77,24 +74,40 @@ class XO:
             return correct_position
 
     def check_win_result(self, position):
-
         for result_list in XO.win_result_list:
             if position in result_list:
                 # если комбинация помечена соперником удаляем её и больше не проверяем
-                if len(result_list) > 3 and result_list[-1] != self.active_player.sign:
+                if not len(result_list[-1]) or self.active_player.sign in result_list[-1]:
+                    result_list[-1].append(self.active_player.sign)
+
+                elif self.active_player.sign not in result_list[-1]:
                     XO.win_result_list.remove(result_list)
-                else:
-                    result_list.append(self.active_player.sign)
-                    if len(result_list) >= 6:
-                        self.winner_detected()
-                        break
 
-        # print(1, XO.win_result_list)
+            if XO.move_counter >= 4:
+                # Есть ли комбинация с 3мя метками
+                if len(result_list[-1]) >= 3:
+                    XO.game_play = False
+                    self.winner_detected(True)
+                    break
 
-    def winner_detected(self):
-        print('winner {}'.format(self.active_player.name))
+                # Ничья
+                elif XO.move_counter >= 7:
+                    XO.game_play = False
+                    self.winner_detected(False)
+                    break
 
+    def winner_detected(self, win):
+        decor = '=' * 20
 
+        print('\n{}'.format(decor))
+        self.board.show_board()
+
+        if win:
+            print('{decor}\nPlayer {player} winer\n{decor}'
+              .format(decor = decor, player = self.active_player.name.upper()))
+        else:
+            print('{decor}\nGAME OVER (DEAD HEAD)\n{decor}'
+                  .format(decor=decor, player=self.active_player.name.upper()))
 
 class Board:
     def __init__(self):
@@ -117,7 +130,7 @@ class Board:
 
     # Проверка ячейки на занятость
     def check_position(self, position):
-        if self.board_list[position[0]][position[1]] != '_':
+        if self.board_list[position[1]][position[0]] != '_':
             print('Увы данная позиция не свободна')
             return False
         else:
@@ -131,8 +144,5 @@ class Player:
 
 if __name__ == '__main__':
     game1 = XO()
-    while True:
+    while game1.game_play:
         game1.player_turn()
-        if game1.finish == True:
-            game1.winner_detected()
-            break
