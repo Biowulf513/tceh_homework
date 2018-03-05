2fd004c224ac642461c2fb292b84e4374e9adfdc
Biowulf <cherepanov92@gmail.com>
2018-03-05 18:11:27 +0500

    Notes day 11 домашнее задание работа над заданием номер 2 #2. Сделать менеджер контекста, который бы проглатывал все исключения вызванные в теле и писал их в консоль,

--- home work/Day11/HomeWork.py
+++ home work/Day11/HomeWork.py
@@ -25,18 +25,39 @@ print('Execution time was:', t.time)
 # Сделать менеджер контекста, который может переопределить значение lock на True внутри блока контекста.
 from contextlib import contextmanager
 
-class Lock(object):
-    def __init__(self):
-        self.lock = False
+# class Lock(object):
+#     def __init__(self):
+#         self.lock = False
+#
+# @contextmanager
+# def locker(some_lock):
+#     some_lock.lock = True
+#     yield some_lock
+#
+# lock1 = Lock()
+# with locker(lock1):
+#     print(lock1.lock)
+#
+# print(lock1.lock)
 
+# -2. Сделать менеджер контекста, который бы проглатывал все исключения вызванные в теле и писал их в консоль,
 @contextmanager
-def locker(some_lock):
-    some_lock.lock = True
-    yield some_lock
+def no_exceptions():
+    message = None
+    try:
+        yield
+    except Exception as E:
+        message = ('log => {}'.format(E))
+    finally:
+        next(no_exceptions)
 
-lock1 = Lock()
-with locker(lock1):
-    print(lock1.lock)
+with no_exceptions():
+    1 / 0  # => logs: ZeroDivisionError
 
-print(lock1.lock)
+with no_exceptions():
+    print('Done!')  # => continues execution
+    'qwe' * 'qwe'  # => logs: ZeroDivisionError
+
+# with no_exceptions():
+
 
